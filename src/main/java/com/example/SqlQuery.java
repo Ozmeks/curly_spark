7fcd56abd15b4f6874785a98593f69cf04ec89f6
omehryakov <alloha23_gh>
2018-09-11 14:56:28 +0500

    New select

--- src/main/java/com/example/SqlQuery.java
+++ src/main/java/com/example/SqlQuery.java
@@ -5,15 +5,19 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+
 public class SqlQuery {
     public static void sqlFromParquet(SparkSession spark, String EventPath) throws AnalysisException {
         // Read a parquet and select from it
         Dataset<Row> countDF =
-                spark.sql("select round(100*count(distinct case when datediff(app.time,reg.time) between 0 and 7 " +
-                        "then reg.email else null end)/count(distinct reg.email),2) as prc " +
+                spark.sql("select round(100*count(distinct case when " +
+                        "cast(app.time as date) between date_add(reg.time,8-date_format(reg.time, 'u')) " +
+                        "and date_add(reg.time,14-date_format(reg.time, 'u')) " +
+                        "then reg.email else null end) " +
+                        "/count(distinct reg.email),2) as prc " +
                         "from parquet.`" + EventPath + "/app_loaded.parquet` as app " +
                         "join parquet.`" + EventPath + "/registered.parquet` as reg " +
-                        "on app.email = reg.email");
+                        "on app.email = reg.email ");
 
         countDF.show();
     }
